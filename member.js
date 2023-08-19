1afaa10f9f76b522948c6f2ff5aa9719aa94f1c5
Muhammad Arslan <123346723+mecxlan@users.noreply.github.com>
2023-08-19 11:10:14 +0500

    Update member.js

--- member.js
+++ member.js
@@ -1,12 +1,12 @@
 function skillsMember() {
   return {
-    restrict: 'E',
-    trmplateUrl: 'modules/skills/views/member.html',
-    controller: 'SkillsMemberController',
-    controllerAs: 'vm',
-    bindToController: true,
+    restrict: 'E',  // Specifies that this directive will be used as an element
+    templateUrl: 'modules/skills/views/member.html',  // Corrected typo in property name
+    controller: 'SkillsMemberController',  // The controller for this directive
+    controllerAs: 'vm',  // Alias for the controller
+    bindToController: true,  // Binds the scope properties to the controller instance
     scope: {
-      member: '='
+      member: '='  // Creates a two-way data binding for the 'member' attribute
     }
   };
 }
